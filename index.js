9ecf19fe3093ec4772fd90896edafc3a8dff9712
WallaR <wallaricsi@gmail.com>
2020-11-09 22:41:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const accordionItem = document.querySelectorAll(".parent");
+
+accordionItem.forEach(parent =>{
+    parent.addEventListener("click", esemeny => {
+        parent.classList.toggle("active");
+    });
+});
